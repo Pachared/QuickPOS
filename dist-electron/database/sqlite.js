2375f7a0f67602029fc3008eb96a39b9b9ef7e10
Axm.pachara <pachared.amr12@gmail.com>
2026-03-17 00:19:44 +0700

    Update sqlite.js

--- dist-electron/database/sqlite.js
+++ dist-electron/database/sqlite.js
@@ -4,7 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const better_sqlite3_1 = __importDefault(require("better-sqlite3"));
-const db = new better_sqlite3_1.default("pos.db");
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+const dataDir = path_1.default.join(process.cwd(), "data");
+if (!fs_1.default.existsSync(dataDir)) {
+    fs_1.default.mkdirSync(dataDir);
+}
+const dbPath = path_1.default.join(dataDir, "pos.db");
+const db = new better_sqlite3_1.default(dbPath);
+db.pragma("journal_mode = WAL");
+db.pragma("foreign_keys = ON");
 db.prepare(`
 CREATE TABLE IF NOT EXISTS products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
