031efae4b69e1eea0776563d4166cf413dcf45e5
Axm.pachara <pachared.amr12@gmail.com>
2026-03-16 12:10:27 +0700

    Create Products.tsx

---
+++ src/pages/Products.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Products() {
+  return (
+    <div>
+      <Typography variant="h5">Product Management</Typography>
+    </div>
+  );
+}
