0bf9eb7f677e8c15514cb3e867ddc6fc4f5733e9
Axm.pachara <pachared.amr12@gmail.com>
2026-03-17 00:19:48 +0700

    Update preload.js

--- dist-electron/preload.js
+++ dist-electron/preload.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const electron_1 = require("electron");
 electron_1.contextBridge.exposeInMainWorld("api", {
+    getProducts: () => electron_1.ipcRenderer.invoke("products:getAll"),
     getProduct: (barcode) => electron_1.ipcRenderer.invoke("product:getByBarcode", barcode),
     createOrder: (cart) => electron_1.ipcRenderer.invoke("order:create", cart),
 });
