3864f4c0f6642f5f8ae2044510f7a9a2aabba3f0
Axm.pachara <pachared.amr12@gmail.com>
2026-03-16 12:10:22 +0700

    Create Orders.tsx

---
+++ src/pages/Orders.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Orders() {
+  return (
+    <div>
+      <Typography variant="h5">Sales History</Typography>
+    </div>
+  );
+}
