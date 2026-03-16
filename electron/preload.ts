fbe76168c0e59f6fbb59c815bc6ff5c4ea2a77e5
Axm.pachara <pachared.amr12@gmail.com>
2026-03-17 00:19:59 +0700

    Update preload.ts

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,8 +1,11 @@
 import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("api", {
+  getProducts: () => ipcRenderer.invoke("products:getAll"),
+
   getProduct: (barcode: string) =>
     ipcRenderer.invoke("product:getByBarcode", barcode),
 
-  createOrder: (cart: any[]) => ipcRenderer.invoke("order:create", cart),
+  createOrder: (cart: any[]) =>
+    ipcRenderer.invoke("order:create", cart),
 });
